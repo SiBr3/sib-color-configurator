da4a20e7c475c84e3ee2ea3d7ad9279c22a0e5ab
SiBr3 <nathankearns@gmail.com>
2026-05-02 15:56:57 +1000

    Add files via upload

---
+++ ui/sib-diplo-fix.js
@@ -0,0 +1,6 @@
+document.head.insertAdjacentHTML('beforeend', `<style>
+    .sib-diplo-override .diplo-ribbon__front-banner {
+        fxs-border-image-tint: var(--player-color-primary);
+    }
+</style>`);
+document.body.classList.add("sib-diplo-override");
