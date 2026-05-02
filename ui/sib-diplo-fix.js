aa22e21f22cd92356695d05c283f060912f3dad2
SiBr3 <nathankearns@gmail.com>
2026-05-02 19:00:47 +1000

    Delete ui/sib-diplo-fix.js

--- ui/sib-diplo-fix.js
+++
@@ -1,6 +0,0 @@
-document.head.insertAdjacentHTML('beforeend', `<style>
-    .sib-diplo-override .diplo-ribbon__front-banner {
-        fxs-border-image-tint: var(--player-color-primary);
-    }
-</style>`);
-document.body.classList.add("sib-diplo-override");
